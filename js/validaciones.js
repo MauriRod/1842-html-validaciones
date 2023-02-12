7e1a4c0530557b6e85e306f0d96eec953ffcb488
MauriRod <maurigugel16@gmail.com>
2023-02-12 17:43:10 -0300

    Implementacion de tipos de error con js

--- js/validaciones.js
+++ js/validaciones.js
@@ -3,10 +3,58 @@ export function valida(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input)
     }
+    console.log(input.parentElement)
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid")
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    }else{
+        input.parentElement.classList.add("input-container--invalid")
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
 }
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacio"
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacio",
+        patternMismatch: "Minimo 8 caracteres y un numero"
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacio",
+        customError: "Debes tener al menos 18"
+    }
+
+};
+
+    
+
 const validadores = {
-    nacimiento: (input) => validarNacimiento(input)
-}
+    nacimiento: (input) => validarNacimiento(input),
+};
+
+function mostrarMensajeDeError(tipoDeInput,input){
+    let mensaje = "";
+    tipoDeErrores.forEach( error =>{
+        if(input.validity[tipoDeInput, error]){
+            console.log(error)
+            console.log(mensajesDeError[tipoDeInput][error])
+            mensaje = mensajesDeError [tipoDeInput][error]
+        }
+    })
+    return mensaje;
+};
+
 function validarNacimiento(input){
     const fechaCliente = new Date(input.value)
     mayorDeEdad(fechaCliente)
